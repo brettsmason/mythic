56ef4b9d219568e6eab681759537509fe3d2fe00
Brett Mason <brettsmason@gmail.com>
2018-12-18 16:29:52 +0000

    Setup menus to work correctly with Foundation.

--- resources/views/nav/menu/default.php
+++ resources/views/nav/menu/default.php
@@ -1,6 +1,6 @@
 <?php if ( has_nav_menu( $data->location ) ) : ?>
 
-	<nav <?php Hybrid\Attr\display( 'menu', $data->location ) ?>>
+	<nav <?php Hybrid\Attr\display( 'menu-wrapper', $data->location ) ?>>
 
 		<h3 class="menu__title screen-reader-text">
 			<?php Hybrid\Menu\display_name( $data->location ) ?>
@@ -10,8 +10,8 @@
 			'theme_location' => $data->location,
 			'container'      => '',
 			'menu_id'        => '',
-			'menu_class'     => 'menu__items',
-			'items_wrap'     => '<ul class="%2$s">%3$s</ul>',
+			'menu_class'     => 'menu vertical medium-horizontal',
+			'items_wrap'     => '<ul class="%2$s" data-responsive-menu="accordion medium-dropdown">%3$s</ul>',
 			'item_spacing'   => 'discard'
 		] ) ?>
 
